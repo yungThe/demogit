be4a397e523f172227d42846fcd7c122670c7fda
yungThe <dungng1536@gmail.com>
2022-02-05 14:08:03 +0700

    asdasfasgasf

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 console.log("asd")
 console.log(5/2)
+console.log(1==1)
+console.log("hello" == "hell")
+console.log("f">="a")
+console.log("so sanh string vs number :","1"== 1)
+console.log("strict compare", "1"===1)
+console.log(Boolean(0))
+let arr =[0, false,"hello",[0,1,2,3]]
+console.log(arr.reverse())
+function fun(a,b){
+    return a+b
+}
+let a=fun(5,10)
+console.log(a)
+const obj = {
+    age: "20",
+    address: "geleximco",
+}
+console.log(obj)
